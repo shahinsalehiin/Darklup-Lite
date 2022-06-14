d7afdf065a6989dc913eec11a399a27ee1a82c04
Engr Md Sazzad Hossian <52770434+sazzadcse@users.noreply.github.com>
2022-06-14 13:46:27 +0600

    version 2.1.0

--- darklup-lite.php
+++ darklup-lite.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Darklup Lite - WP Dark Mode
  * Plugin URI:        https://darklup.com/
  * Description:       All in one WordPress plugin to create a stunning dark version for your WordPress website and dashboard
- * Version:           2.0.9
+ * Version:           2.1.0
  * Author:            Darklup
  * Author URI:        https://darklup.com/
  * License:           GPL v2 or later
@@ -27,7 +27,7 @@ if( !defined( 'DARKLUPLITE_ALERT_MSG' ) ) {
 
 // Version constant
 if( !defined( 'DARKLUPLITE_VERSION' ) ) {
-	define( 'DARKLUPLITE_VERSION', '2.0.9' );
+	define( 'DARKLUPLITE_VERSION', '2.1.0' );
 }
 
 // Plugin dir path constant
